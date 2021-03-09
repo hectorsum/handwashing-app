edb0a10b3db920b32bac6f6c9bc5c0da3080e116
Hector Herrera <hectorsum81@hotmail.com>
2021-03-09 10:48:59 -0500

    structure done

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Timer from './Timer.svelte';
+  import HowTo from './HowTo.svelte'
+</script>
+<style>
+  h1,h3{
+    text-align: center;
+  }
+</style>
+<h1>Handwashing App</h1>
+<Timer/>
+<HowTo />
+
+<h3>
+  <a href="http://who.int/gpsc/clean_hands_protection/en/">
+    Picture source
+  </a>
+  <a href="https://freesound.org/people/metrostock99/sounds/345086/">
+    Sound source
+  </a>
+</h3>
